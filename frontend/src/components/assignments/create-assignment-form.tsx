e75dbf75b1c4663a59b9af1aaf69cde18a3ea112
sharvarianand <sharvaribhondekar23@gmail.com>
2026-05-26 13:27:12 +0530

    feat: change class dropdown to use roman numerals

--- frontend/src/components/assignments/create-assignment-form.tsx
+++ frontend/src/components/assignments/create-assignment-form.tsx
@@ -236,7 +236,7 @@ export function CreateAssignmentForm() {
                   className="h-11 w-full bg-transparent text-[14px] text-ink focus:outline-none cursor-pointer"
                 >
                   <option value="" disabled>Select a class</option>
-                  {["Nursery", "LKG", "UKG", "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th"].map(c => (
+                  {["Nursery", "LKG", "UKG", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"].map(c => (
                     <option key={c} value={c}>{c}</option>
                   ))}
                 </select>
